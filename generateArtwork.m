3560431382c723707eb347840cfa0dd43bfced62
Lane McIntosh <lanemcintosh@gmail.com>
2013-12-20 11:17:59 -0800

    added the MATLAB script to generate my 'artwork'

---
+++ generateArtwork.m
@@ -0,0 +1,11 @@
+%generateArtwork.m
+
+% Create a surface plot using the surf function
+figure;
+surf(rand(10),rand(10),rand(10))
+
+
+% Turn off the axis and the grid
+%axis('square', 'off');
+grid('off');
+axis('off')
