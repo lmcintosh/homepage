0d7d650bfcab07b25d234b34823855510b983e8d
Lane McIntosh <lanemcintosh@gmail.com>
2013-12-24 21:50:24 -0800

    Updated generateArtwork script to accurately reflect surface-contour plot

--- generateArtwork.m
+++ generateArtwork.m
@@ -2,9 +2,12 @@
 
 % Create a surface plot using the surf function
 figure;
-numPoints = 25;  % looks good from 10 to 25
-surf(rand(numPoints),rand(numPoints),rand(numPoints))
-%surfc(randn(numPoints),randn(numPoints),randn(numPoints)) % w contour plot
+%numPoints = 12;  % looks good from 10 to 25
+%surf(rand(numPoints),rand(numPoints),rand(numPoints))
+
+numPoints1 = 25;
+numPoints2 = 10;
+surfc(linspace(0,1,numPoints2),linspace(0,1,numPoints1),rand(numPoints1,numPoints2)) % w contour plot
 
 
 % Turn off the axis and the grid
